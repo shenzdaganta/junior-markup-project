2359d885caad2e7f7b730a0ef8a8511ed64a8f25
shenzdaganta <143105468+shenzdaganta@users.noreply.github.com>
2023-12-05 14:46:51 +0000

    Deploying to gh-pages from @ shenzdaganta/junior-markup-project@870fa92c736c7eb22c3a47149b7873bdf9e30141 🚀

---
+++ index.d212fa06.js
@@ -0,0 +1,2 @@
+(()=>{let e={openModalBtn:document.querySelector("[data-modal-open]"),closeModalBtn:document.querySelector("[data-modal-close]"),modal:document.querySelector("[data-modal]")};function o(){e.modal.classList.toggle("is-hidden")}e.openModalBtn.addEventListener("click",o),e.closeModalBtn.addEventListener("click",o)})();
+//# sourceMappingURL=index.d212fa06.js.map
