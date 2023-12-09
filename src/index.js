6a585e8b45721857d629b9386ef75b91929b87f1
shenzdaganta <143105468+shenzdaganta@users.noreply.github.com>
2023-12-09 21:05:52 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+//Hero modal//
+
 (() => {
   const refs = {
     openModalBtn: document.querySelector("[data-modal-open]"),
@@ -130,4 +132,19 @@ document.addEventListener("DOMContentLoaded", function () {
         // Initial display
         showCards(currentIndex);
         updateDots(currentIndex);
-    });
+});
+    
+//subscribe modal//
+
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector("[data-modal-open]"),
+    closeModalBtn: document.querySelector("[data-modal-close]"),
+    modal: document.querySelector("[data-modal]"),
+  };
+  refs.openModalBtn.addEventListener("click", toggleModal);
+  refs.closeModalBtn.addEventListener("click", toggleModal);
+  function toggleModal() {
+    refs.modal.classList.toggle("is-hidden");
+  }
+})();
