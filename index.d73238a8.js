6710655014d699f137175e859054d361d868602d
shenzdaganta <143105468+shenzdaganta@users.noreply.github.com>
2023-12-10 15:07:59 +0000

    Deploying to gh-pages from @ shenzdaganta/junior-markup-project@c2216e7612a7e0db52a39aecf9fc63c85bb60cb8 🚀

---
+++ index.d73238a8.js
@@ -0,0 +1,2 @@
+(()=>{let e={openModalBtn:document.querySelector("[data-modal-open-review]"),closeModalBtn:document.querySelector("[data-modal-close-review]"),modal:document.querySelector("[data-modal-review]")};function o(){e.modal.classList.toggle("is-hidden")}e.openModalBtn.addEventListener("click",o),e.closeModalBtn.addEventListener("click",o)})();
+//# sourceMappingURL=index.d73238a8.js.map
