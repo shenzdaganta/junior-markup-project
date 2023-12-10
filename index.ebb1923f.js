5f6ba367e3a9a2b2da00fab6bc24cee2aa3fc3ad
shenzdaganta <143105468+shenzdaganta@users.noreply.github.com>
2023-12-10 11:16:18 +0000

    Deploying to gh-pages from @ shenzdaganta/junior-markup-project@5d7adc067c725e6261dcbb05e9900023731b1c58 🚀

---
+++ index.ebb1923f.js
@@ -0,0 +1,2 @@
+new Swiper(".mySwiper",{slidesPerView:3,spaceBetween:20,slidesPerGroup:1,loop:!0,fade:!0,centerSlider:!0,grabCursor:!0,pagination:{el:".swiper-pagination",clickable:!0,dynamicBullets:!0},navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev"},breakpoints:{0:{slidesPerView:1},768:{slidesPerView:2},1200:{slidesPerView:3}}});
+//# sourceMappingURL=index.ebb1923f.js.map
